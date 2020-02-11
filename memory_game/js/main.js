927ad56875cc5aff98fd94f4c2073c145cbd4529
Abdulaziz <labdul2ziz@gmail.com>
2020-02-12 02:44:37 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "";
-let cardFour = "";
+const cards = ["queen" ,"queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    } else {
+        alert("Sorrt, try again.");
+    }
+}
